4c09bd240457f1fde81dbc5d9837000dd0b7d6f3
mikesaprykin <mikesaprykin@gmail.com>
2017-07-12 22:38:07 +0300

    feat(confirm-ending): Updated test and function docs
    Added task description

--- src/free_code_camp/basic-algorithm-scripting/confirm-ending/confirm-ending.js
+++ src/free_code_camp/basic-algorithm-scripting/confirm-ending/confirm-ending.js
@@ -1,6 +1,7 @@
 /**
  * Confirm ending function, that takes a string and an ending.
  * Returns a boolean if string is ending on the string, passed as ending param
+ *
  * @param string
  * @param ending
  * @return {boolean}
