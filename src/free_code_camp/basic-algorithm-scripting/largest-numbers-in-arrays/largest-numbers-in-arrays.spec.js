d4f1f414dc48419614a673288d1e97018ec5025d
mikesaprykin <mikesaprykin@gmail.com>
2017-07-12 19:09:09 +0300

    feat(largest-numbers-in-arrays): Added largest-numbers-in-arrays function
    Updated package and readme for a new version. Also created a folder for largest-numbers-in-arrays
    task with tests, both declarative and imperative

---
+++ src/free_code_camp/basic-algorithm-scripting/largest-numbers-in-arrays/largest-numbers-in-arrays.spec.js
@@ -0,0 +1,38 @@
+import {
+    largestOfFour,
+    largestOfFour2
+} from './largest-numbers-in-arrays';
+import testParams from './test.params';
+
+/**
+ * TASK: Return an array consisting of the largest number from
+ * each provided sub-array. For simplicity, the provided
+ * array will contain exactly 4 sub-arrays.
+ */
+describe('largestOfFour function tests, declarative ', () => {
+
+    it('should be defined', () => {
+        expect(largestOfFour).toBeDefined();
+    });
+
+    testParams.forEach(({ param, result }) => {
+        it(`should return ${JSON.stringify(result)} using ${JSON.stringify(param)}`, () => {
+            expect(largestOfFour(param)).toEqual(result)
+        })
+    });
+
+});
+
+describe('largestOfFour2 function tests, imperative ', () => {
+
+    it('should be defined', () => {
+        expect(largestOfFour2).toBeDefined();
+    });
+
+    testParams.forEach(({ param, result }) => {
+        it(`should return ${JSON.stringify(result)} using ${JSON.stringify(param)}`, () => {
+            expect(largestOfFour2(param)).toEqual(result)
+        })
+    });
+
+});
