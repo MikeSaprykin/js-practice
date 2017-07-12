83db01d7251df8d4a0fb9f89ae11448cc3853e27
mikesaprykin <mikesaprykin@gmail.com>
2017-07-12 22:38:07 +0300

    feat(confirm-ending): Updated test and function docs
    Added task description

--- src/free_code_camp/basic-algorithm-scripting/confirm-ending/confirm-ending.spec.js
+++ src/free_code_camp/basic-algorithm-scripting/confirm-ending/confirm-ending.spec.js
@@ -1,6 +1,14 @@
 import { confirmEnding } from './confirm-ending';
 import testParams from './test.params';
 
+/**
+ * TASK: Check if a string (first argument, string)
+ * ends with the given target string (second argument, ending).
+ * This challenge can be solved with the .endsWith()
+ * method, which was introduced in ES2015. But for the purpose
+ * of this challenge, we would like you to use one of
+ * the JavaScript substring methods instead.
+ */
 describe('confirmEnding function tests: ', () => {
 
     it('should be defined', () => {
