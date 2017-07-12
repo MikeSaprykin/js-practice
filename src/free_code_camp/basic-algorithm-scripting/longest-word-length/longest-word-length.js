52bb98e8099d1b4bd6512dfa677bf739c64256c4
mikesaprykin <mikesaprykin@gmail.com>
2017-07-12 18:15:53 +0300

    feat(longest-word-length): Added imperative function
    Added function of longest-word-length in imperative style, tested

--- src/free_code_camp/basic-algorithm-scripting/longest-word-length/longest-word-length.js
+++ src/free_code_camp/basic-algorithm-scripting/longest-word-length/longest-word-length.js
@@ -4,6 +4,26 @@
  * @param str
  * @returns {*}
  */
-export const findLongestWordsLength = (str) => {
+export const findLongestWordLength = (str) => {
     return str.split(' ').map(word => word.length).sort((a, b) => b - a)[0]
+};
+
+/**
+ * Function to get the length of the longest word
+ * Imperative style
+ * @param str
+ * @returns {number}
+ */
+export const findLongestWordLength2 = (str) => {
+    const splitString = str.split(' ');
+
+    let longestWordLength = 0;
+
+    for(let i = 0; i < splitString.length; i++) {
+        let latestLength = splitString[i].length;
+        if(latestLength > longestWordLength) {
+            longestWordLength = latestLength;
+        }
+    }
+    return longestWordLength;
 };
--- src/free_code_camp/basic-algorithm-scripting/longest-word-length/longest-word-length.spec.js
+++ src/free_code_camp/basic-algorithm-scripting/longest-word-length/longest-word-length.spec.js
@@ -1,15 +1,32 @@
-import { findLongestWordsLength } from './longest-word-length';
+import {
+    findLongestWordLength,
+    findLongestWordLength2
+} from './longest-word-length';
 import testParams from './test.params';
 
-describe('Longest word length function tests ', () => {
+describe('findLongestWordLength function tests, declarative style ', () => {
 
     it('should be defined ', () => {
-       expect(findLongestWordsLength).toBeDefined()
+       expect(findLongestWordLength).toBeDefined()
     });
 
     it('should return expected length of the word using the params equal to each result', () => {
         testParams.forEach(({param, result}) => {
-            expect(findLongestWordsLength(param)).toEqual(result)
+            expect(findLongestWordLength(param)).toEqual(result)
+        });
+    });
+
+});
+
+describe('findLongestWordLength2 function tests, imperative style ', () => {
+
+    it('should be defined ', () => {
+        expect(findLongestWordLength2).toBeDefined()
+    });
+
+    it('should return expected length of the word using the params equal to each result', () => {
+        testParams.forEach(({param, result}) => {
+            expect(findLongestWordLength2(param)).toEqual(result)
         });
     });
 
