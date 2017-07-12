0023f79075dc42de3106634f5fab7f08c0686809
mikesaprykin <mikesaprykin@gmail.com>
2017-07-12 22:56:09 +0300

    feat(repeat-string.spec): Updated spec description
    Added task description

--- src/free_code_camp/basic-algorithm-scripting/repeat-string/repeat-string.spec.js
+++ src/free_code_camp/basic-algorithm-scripting/repeat-string/repeat-string.spec.js
@@ -1,6 +1,11 @@
 import { repeatStringNumTimes } from './repeat-string';
 import testParams from './test.params';
 
+/**
+ * TASK: Repeat a given string (first argument)
+ * times times (second argument).
+ * Return an empty string if num is not a positive number.
+ */
 describe('repeatStringNumTimes function test ', () => {
 
     it('should be defined', () => {
