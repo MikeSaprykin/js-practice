4acdc2fee3ea8723d11953684d29cfd3bcd9d007
mikesaprykin <mikesaprykin@gmail.com>
2017-07-14 11:42:18 +0300

    feat(falsy-bouncer): Falsy bouncer function and tests added, readme updated
    Updated readme with falsy-bouncer task folder. Created simple function for filtering falsy values

--- src/free_code_camp/basic-algorithm-scripting/falsy-bouncer/test.params.js
+++ src/free_code_camp/basic-algorithm-scripting/falsy-bouncer/test.params.js
@@ -1,3 +1,18 @@
 export default [
-
+    {
+        param: [7, "ate", "", false, 9],
+        result: [7, "ate", 9]
+    },
+    {
+        param: ["a", "b", "c"],
+        result: ["a", "b", "c"]
+    },
+    {
+        param: [false, null, 0, NaN, undefined, ""],
+        result: []
+    },
+    {
+        param: [1, null, NaN, 2, undefined],
+        result: [1, 2]
+    }
 ]
