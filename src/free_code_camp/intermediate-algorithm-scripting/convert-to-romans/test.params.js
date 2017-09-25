5b6dbf0c2d1943a775f25f166038f936bbe5d1aa
mikesaprykin <mikesaprykin@gmail.com>
2017-09-25 11:12:56 +0300

    feat(convert-to-romans): Added convert to romans task and function

---
+++ src/free_code_camp/intermediate-algorithm-scripting/convert-to-romans/test.params.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        param: [2],
+        result: ['II']
+    },
+    {
+        param: [4],
+        result: ['IV']
+    },
+    {
+        param: [5],
+        result: ['V']
+    },
+    {
+        param: [9],
+        result: ['IX']
+    },
+    {
+        param: [29],
+        result: ['XXIX']
+    },
+    {
+        param: [68],
+        result: ['LXVIII']
+    },
+    {
+        param: [500],
+        result: ['D']
+    },
+    {
+        param: [891],
+        result: ['DCCCXCI']
+    },
+    {
+        param: [1023],
+        result: ['MXXIII']
+    },
+    {
+        param: [3999],
+        result: ['MMMCMXCIX']
+    }
+]
